10a83229783055c872154bf11d88165a25922dcb
Nick W <59586563+nwweis@users.noreply.github.com>
2022-02-28 17:50:23 +0800

    Update settings.py
    Removed secret key

--- libmgmt/libmgmt/settings.py
+++ libmgmt/libmgmt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9(j)jw!13+3cebrf%7o(-a3k)piq$&*npzq$zqu(+_n2w+3bmk'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
